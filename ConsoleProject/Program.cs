dbc0b11e6d88552c544ffcad5d04d29211f3c28e
varun-planview <varun.kumar@planview.com>
2023-02-06 17:16:22 +0530

    Encrypt-Decrypt text using salt, IV & key

---
+++ ConsoleProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleProject;
+
+Console.WriteLine("Please enter a password to use:");
+string password = Console.ReadLine();
+while (true)
+{
+    Console.WriteLine("Please enter a string to encrypt or e to exit:");
+    string plaintext = Console.ReadLine();
+    if (plaintext == "e")
+        break;
+    Console.WriteLine("");
+
+    Console.WriteLine("Your encrypted string is:");
+    string encryptedstring = StringCipher.Encrypt(plaintext, password);
+    Console.WriteLine(encryptedstring);
+    Console.WriteLine("");
+
+    Console.WriteLine("Your decrypted string is:");
+    string decryptedstring = StringCipher.Decrypt(encryptedstring, password);
+    Console.WriteLine(decryptedstring);
+    Console.WriteLine("");
+}
